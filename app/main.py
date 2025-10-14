d2b3a07744f8a516b4cf898389ea502871621087
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-10-14 09:03:45 +0600

    updated

--- app/main.py
+++ app/main.py
@@ -40,6 +40,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    allowed_hosts=["*"]
 )
 
 # Set up trusted host middleware
